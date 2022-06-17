edea9eb2bd92ff26643e0a1407f9af4543849717
Sam.Donaghy-Bell.76008920 <Sam.Donaghy-Bell@microfocus.com>
2022-06-17 12:16:37 +0100

    usestate and useeffect hooks now used instead of this.setState

--- src/App.js
+++ src/App.js
@@ -1,82 +1,68 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import Scroll from "./Scroll";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      characters: [],
-      searchfield: "",
-    };
-  }
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
+
+  useEffect(() => {
+    fetch("https://hp-api.herokuapp.com/api/characters")
+      .then((response) => response.json())
+      .then((charactersAll) => {
+        setCharacters(charactersAll);
+      });
+  }, []);
+
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
   };
 
-  async componentDidMount() {
-    try {
-      const res = await fetch("https://hp-api.herokuapp.com/api/characters");
-      const charactersAll = await res.json();
-      this.setState({ characters: charactersAll });
-    } catch (error) {
-      console.log({ error });
-    }
-  }
 
-
-
-  render() {
-    if (this.state.characters?.length > 0) {
-      const filteredCharacters = this.state.characters?.filter((character) => {
-        return (
-          character.name
-            .toLowerCase()
-            .startsWith(this.state.searchfield?.toLowerCase()) ||
-          character.house
-            .toLowerCase()
-            .startsWith(this.state.searchfield?.toLowerCase()) ||
-          character.wand.wood
-            .toLowerCase()
-            .startsWith(this.state.searchfield?.toLowerCase())
-            ||
-          character.wand.core
-            .toLowerCase()
-            .startsWith(this.state.searchfield?.toLowerCase())
-            ||
-          character.ancestry
-            .toLowerCase()
-            .startsWith(this.state.searchfield?.toLowerCase())
-            ||
-            character.dateOfBirth
-              .toLowerCase()
-              .includes(this.state.searchfield?.toLowerCase())
-        );
-      });
-
+  if (characters?.length > 0) {
+    const filteredCharacters = characters?.filter((character) => {
       return (
-        <div className="container flex flex-col items-center justify-center mx-auto">
-          <h1 className="font-bold text-white text-2xl mt-4">
-            Harry Potter Characters
-          </h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList characters={filteredCharacters} />
-          </Scroll>
-        </div>
+        character.name.toLowerCase().startsWith(searchfield?.toLowerCase()) ||
+        character.house.toLowerCase().startsWith(searchfield?.toLowerCase()) ||
+        character.wand.wood
+          .toLowerCase()
+          .startsWith(searchfield?.toLowerCase()) ||
+        character.wand.core
+          .toLowerCase()
+          .startsWith(searchfield?.toLowerCase()) ||
+        character.ancestry
+          .toLowerCase()
+          .startsWith(searchfield?.toLowerCase()) ||
+        character.dateOfBirth.toLowerCase().includes(searchfield?.toLowerCase())
       );
-    } else {
-      return (
-        <>
+    });
+
+    return (
+      <div className="container flex flex-col items-center justify-center mx-auto">
+        <h1 className="font-bold text-white text-2xl mt-4">
+          Harry Potter Characters
+        </h1>
+        
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <CardList characters={filteredCharacters} />
+        </Scroll>
+      </div>
+    );
+  } else {
+    return (
+      <>
         <h1 className="font-bold text-center text-white text-2xl mt-4">
-            Harry Potter Characters
-          </h1>
-        <p className='text-white text-xl text-center mt-12'>Loading characters...</p>
-        </>
-      )
-    }
+          Harry Potter Characters
+        </h1>
+        <p className="text-white text-xl text-center mt-12">
+          Loading characters...
+        </p>
+      </>
+    );
   }
 }
 
